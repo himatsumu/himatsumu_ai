0d7e8f9a152ef3e46a30641066b3e204cb932b7a
tapipipipi <y.tani.1226@gmail.com>
2025-06-26 12:05:06 +0900

    add detailsAPIのテストファイル

---
+++ tests/test_recommend/details_test.py
@@ -0,0 +1,22 @@
+import googlemaps
+import json
+from config.settings import get_setting
+
+# APIキーの取得
+settings = get_setting()
+client = googlemaps.Client(settings.GOOGLE_MAP_KEY) #インスタンス生成
+
+
+# Place ID を指定（例: 中崎町の服屋など）
+place_id = 'ChIJaxLzYf3nAGARD6HBizIB5_g'  # ※適当なPlace IDに置き換えてください
+
+# 詳細取得
+place_details = client.place(place_id)
+
+# 出力
+from pprint import pprint
+pprint(place_details["result"])
+
+# ダミーデータ作成用：重複なしの結果を新しいJSONに保存
+with open(settings.MOCK_DETAILE_JSON, "w", encoding="utf-8") as f:
+    json.dump(place_details, f, ensure_ascii=False, indent=2)
